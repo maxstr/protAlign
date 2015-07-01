3cbfe47cd02b4e4be5595e41a866ff69f11920c3
Max Stritzinger <mstritzinger@live.com>
2015-07-01 15:11:56 -0600

    Final changes to protAlign

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # protAlign
 
 Runs TM-align and extracts RMSD and TMScore for models against native structures. 
+
+#TM-Align
+Information about TM-Align can be found [here](http://zhanglab.ccmb.med.umich.edu/TM-align/)
+
+# Requirements
+
+- Python
--- protAlign.py
+++ protAlign.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python
 from fabric.api import local, env, settings
+from fabric.context_managers import hide
 import numpy
 import re
-from itertools import chain
+from itertools import chain, combinations
 from os import listdir, makedirs
 from os.path import isfile, join, normpath, exists, abspath
 from sys import exit
 import csv
 import sys
 import os
-
+from pprint import pprint
 
 # ./protAlign seqDirectory nativepdb [destination]
 
@@ -17,12 +19,15 @@ def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
 # Runs TM-align on two pdb files
-def tmAlign(file1, file2, tmPath):
+def tmAlign(file1, file2):
     tmPath = join(os.path.dirname(os.path.realpath(__file__)), 'TMalign')
-    with settings(capture=True):
-        return local("%s %s %s" % (tmPath, file1, file2))
-# Extract RMSD from TM-align output
+    with hide('running'):
+        ret = local("%s %s %s" % (tmPath, file1, file2), capture=True)
+    return ret
+
+# Extract RMSD/TMExtract from TM-align output
 def rmsdExtract(tmOutput):
     return re.search("RMSD= *([0-9.]*)", tmOutput).group(1)
 
@@ -38,6 +43,7 @@ def main(seqDirectory, nativePDB, destination):
             exit("Couldn't create destination directories and they don't exist!")
 
     results = {}
+    results['seq'] = seqDirectory
     dataDir = join(destination, 'data')
     results['dataDir'] = dataDir
     # First we handle all model-model comparisons
@@ -54,8 +60,8 @@ def main(seqDirectory, nativePDB, destination):
         rmsd = rmsdExtract(tmOutput)
         tmScores = tmExtract(tmOutput)
         modelsResultsDict[pair] = { 'rmsd': rmsd, 'tmScore-1': tmScores[0], 'tmScore-2':tmScores[1], 'pair':f }
-        with open(abspath(join(dataDir, "%s-%s-align.txt" % (model1, model2))), 'w') as f:
-            f.write(tmOutput)
+        with open(abspath(join(dataDir, "%s-%s-align.txt" % (model1.replace(".pdb", ""), model2.replace(".pdb", "")))), 'w') as f:
+            f.write(tmOutput + "\n")
 
     # Now we need to calculate STD Deviation, Mean for these values.
     # First get list of all RMSDs, TMScores
@@ -66,7 +72,7 @@ def main(seqDirectory, nativePDB, destination):
     results['stdModelsRMSD'] = numpy.std(modelsRMSD)
     results['meanModelsRMSD'] = numpy.mean(modelsRMSD)
     results['stdModelsTMScore'] = numpy.std(modelsTMScore)
-    results['meanModelsTMScore'] = numpy.std(modelsTMScore)
+    results['meanModelsTMScore'] = numpy.mean(modelsTMScore)
 
     # Now we do model-native comparisons
     modelNativeResultsDict = {}
@@ -75,8 +81,8 @@ def main(seqDirectory, nativePDB, destination):
         rmsd = rmsdExtract(tmOutput)
         tmScores = tmExtract(tmOutput)
         modelNativeResultsDict[i] = { 'rmsd': rmsd, 'tmScore-1': tmScores[0], 'tmScore-2':tmScores[1], 'pair':i }
-        with open(abspath(join(dataDir, "%s-%s-align.txt" % ('native', i))), 'w') as f:
-            f.write(tmOutput)
+        with open(abspath(join(dataDir, "%s-%s-align.txt" % ('native', i.replace(".pdb", "")))), 'w') as f:
+            f.write(tmOutput + "\n")
 
     modelNativeRMSD = [ float(f[1]['rmsd']) for f in modelNativeResultsDict.items() ]
     # Here we *must* use the first TM-Score, as it is the TM Score associated with using the native struct as the reference.
@@ -85,38 +91,42 @@ def main(seqDirectory, nativePDB, destination):
     results['stdModelNativeRMSD'] = numpy.std(modelNativeRMSD)
     results['meanModelNativeRMSD'] = numpy.mean(modelNativeRMSD)
     results['stdModelNativeTMScore'] = numpy.std(modelNativeTMScore)
-    results['meanModelNativeTMScore'] = numpy.std(modelNativeTMScore)
+    results['meanModelNativeTMScore'] = numpy.mean(modelNativeTMScore)
     # Now we find the model with the lowest RMSD, TMScore
-    results['minRMSDModel'] = min(modelNativeResultsDict, key=lambda model:ModelNativeResultsDict[model]['rmsd'])
-    results['minRMSValue'] = modelNativeResultsDict[minRMSModel]['rmsd']
-    results['maxTMScoreModel'] = max(modelNativeResultsDict, key =lambda model:ModelNativeResultsDict[model]['tmScore-1'])
-    results['maxTMScoreValue'] = modelNativeResultsDict[maxTMScoreValue]['tmScore-1']
+    results['minRMSDModel'] = min(modelNativeResultsDict, key=lambda model:float(modelNativeResultsDict[model]['rmsd']))
+    minRMSDModel = results['minRMSDModel']
+    results['minRMSDValue'] = float(modelNativeResultsDict[minRMSDModel]['rmsd'])
+    results['maxTMScoreModel'] = max(modelNativeResultsDict, key =lambda model:float(modelNativeResultsDict[model]['tmScore-1']))
+    maxTMScoreModel = results['maxTMScoreModel']
+    results['maxTMScoreValue'] = float(modelNativeResultsDict[maxTMScoreModel]['tmScore-1'])
 
     # We have all of our values, time to write the file.
-    output = """
-    Align Output for %s
+    output = """\
+Align Output for %(seq)s
 
-    Number of Models: %(numModels)i
-    Data Output: %(dataDir)s
+Number of Models: %(numModels)i
+Data Output: %(dataDir)s
 
-    Model-Model Comparisons
-    TMScore Mean: %(meanModelsTMScore)f
-    TMScore STD: %(stdModelsTMScore)f
+Model-Model Comparisons
 
-    RMSD Mean: %(meanModelsRMSD)f
-    RMSD STD: %(stdModelsRMSD)f
+TMScore Mean: %(meanModelsTMScore)f
+TMScore STD: %(stdModelsTMScore)f
 
-    Native-Model Comparisons
-    TMScore Mean: %(meanModelNativeTMScore)f
-    TMScore STD: %(stdModelNativeTMScore)f
-    TMScore Max (model): %(maxTMScoreValue) ( %(maxTMScoreModel) )
+RMSD Mean: %(meanModelsRMSD)f
+RMSD STD: %(stdModelsRMSD)f
 
-    RMSD Mean: %(meanModelNativeRMSD)f
-    RMSD STD: %(stdModelNativeRMSD)f
-    RMSD Min (model): %(minRMSDValue)f ( %(minRMSDModel)s )
-    """
+Native-Model Comparisons
 
-    with open(abspath(join(destination, "report.txt"))), 'w') as report:
+TMScore Mean: %(meanModelNativeTMScore)f
+TMScore STD: %(stdModelNativeTMScore)f
+TMScore Max (model): %(maxTMScoreValue)f ( %(maxTMScoreModel)s )
+
+RMSD Mean: %(meanModelNativeRMSD)f
+RMSD STD: %(stdModelNativeRMSD)f
+RMSD Min (model): %(minRMSDValue)f ( %(minRMSDModel)s )
+"""
+
+    with open(abspath(join(destination, "report.txt")), 'w') as report:
         report.write(output % results)
 
 
